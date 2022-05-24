4f948fa69774bdc73daf926ae90a1df617c79fb2
Dawid Brzozowski <dawidbrzozowski1998@gmail.com>
2022-05-24 22:51:12 +0200

    feat: fix table orders

--- src/components/orders/tableOrders.tsx
+++ src/components/orders/tableOrders.tsx
@@ -1,18 +1,16 @@
-import { Button, Space, Table } from "antd"
+import { Button, Table } from "antd"
 import axios from "axios"
 import { useEffect, useState } from "react"
 import environment from "../../environments/environment.json"
 import useAxiosAuth from "../../hooks/useAxiosAuth"
-import { AddMenuItemModal } from "../modals/addMenuItemModal"
 import failNotification from "../notifications/fail/failNotification"
 import successNotification from "../notifications/success/successNotification"
-import {IMenuItem} from "../menuItem/menuItems";
 
 
 export interface ITable {
     id: string,
-    name: string,
-    menuItems: Array<IMenuItem>
+    tableId: string,
+    menuItems: string
 }
 
 
@@ -27,26 +25,17 @@ const TableOrders = () => {
             key: "name"
         },
         {
-            title: "Kategoria",
-            dataIndex: "category",
-            key: "category"
-        },
-        {
-            title: "Cena",
-            dataIndex: "price",
-            key: "price"
-        },
-        {
-            title: "Opis",
-            dataIndex: "description",
-            key: "description"
+            title: "Status",
+            dataIndex: "status",
+            key: "status"
         }
     ]
 
     const handleDelete = async (id: string) => {
         axiosAuth
-            .delete(`Admin/DeleteTableOrder/${id}`)
+            .post(`Admin/IssueOrder`, {tableId: parseInt(id)})
             .then(() => {
+                setTableOrders(tableOrders?.filter((item: ITable) => item.tableId !== id))
                 successNotification("The table order has been removed.")
             })
             .catch(() => {
@@ -54,14 +43,19 @@ const TableOrders = () => {
             })
     }
 
+    const toDataSource = (menuItem: string) => {
+        let menuItems = menuItem.split(";")
+        return menuItems.map(el => ({name: el, status: "In progress"}))
+    }
+
     const renderSingleTable = (table: ITable) => {
         return (
             <div style={{margin: 30}}>
-                <h2>{table.name}</h2>
-                <Table columns={columns} dataSource={table.menuItems}/>
+                <h2>{`Stolik #${table.tableId}`}</h2>
+                <Table columns={columns} dataSource={toDataSource(table.menuItems)}/>
                 <Button type="primary"
                         danger ghost
-                        onClick={() => handleDelete(table.id)}
+                        onClick={() => handleDelete(table.tableId)}
                         style={{width: 200, justifyContent: 'center'}}
                 >
                     Zresetuj stolik
@@ -75,72 +69,72 @@ const TableOrders = () => {
         downloadTableOrders()
     }, [environment.apiUri + "TableOrders"])
 
-    //
-    // const downloadTableOrders = async () => {
-    //     axios.get<ITable[]>(environment.apiUri + "TableOrders")
-    //         .then(res => {
-    //             const data = res.data
-    //                 .map((item: ITable) => ({ ...item, key: item.id }))
-    //                 .sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
-    //             setTableOrders(data)
-    //         })
-    //         .catch(() => {
-    //             failNotification("Couldn't download the list of table orders.")
-    //         })
-    // }
-    const downloadTableOrders = () => {
-        setTableOrders(
-            [
-                {
-                    "id": "1",
-                    "name": "Stolik #1",
-                    "menuItems": [
-                        {
-                            id: "1",
-                            name: "Margherita",
-                            category: "Pizza",
-                            price: 27,
-                            description: "Good pizza"
-                        },
-                        {
-                            id: "5",
-                            name: "Salame Picante",
-                            category: "Pizza",
-                            price: 37,
-                            description: "Good pizza"
-                        },
-                    ]
-                },
-                {
-                    "id": "2",
-                    "name": "Stolik #2",
-                    "menuItems": [
-                        {
-                            id: "2",
-                            name: "Hawajska",
-                            category: "Pizza",
-                            price: 34,
-                            description: "Good pizza"
-                        }
-                    ]
-                },
-                {
-                    "id": "3",
-                    "name": "Stolik #3",
-                    "menuItems": [
-                        {
-                            id: "1",
-                            name: "Prosciutto",
-                            category: "Pizza",
-                            price: 28,
-                            description: "good pizza"
-                        }
-                    ]
-                },
 
-            ]
-        )
+    const downloadTableOrders = async () => {
+        axiosAuth.get<ITable[]>(environment.apiUri + `Admin/GetOrders`)
+            .then(res => {
+                const data = res.data
+                    .map((item: ITable) => ({ ...item, key: item.id }))
+                    .sort((a, b) => (a.tableId > b.tableId) ? 1 : ((b.tableId > a.tableId) ? -1 : 0))
+                setTableOrders(data)
+            })
+            .catch(() => {
+                failNotification("Couldn't download the list of table orders.")
+            })
     }
+    // const downloadTableOrders = () => {
+    //     setTableOrders(
+    //         [
+    //             {
+    //                 "id": "1",
+    //                 "name": "Stolik #1",
+    //                 "menuItems": [
+    //                     {
+    //                         id: "1",
+    //                         name: "Margherita",
+    //                         category: "Pizza",
+    //                         price: 27,
+    //                         description: "Good pizza"
+    //                     },
+    //                     {
+    //                         id: "5",
+    //                         name: "Salame Picante",
+    //                         category: "Pizza",
+    //                         price: 37,
+    //                         description: "Good pizza"
+    //                     },
+    //                 ]
+    //             },
+    //             {
+    //                 "id": "2",
+    //                 "name": "Stolik #2",
+    //                 "menuItems": [
+    //                     {
+    //                         id: "2",
+    //                         name: "Hawajska",
+    //                         category: "Pizza",
+    //                         price: 34,
+    //                         description: "Good pizza"
+    //                     }
+    //                 ]
+    //             },
+    //             {
+    //                 "id": "3",
+    //                 "name": "Stolik #3",
+    //                 "menuItems": [
+    //                     {
+    //                         id: "1",
+    //                         name: "Prosciutto",
+    //                         category: "Pizza",
+    //                         price: 28,
+    //                         description: "good pizza"
+    //                     }
+    //                 ]
+    //             },
+    //
+    //         ]
+    //     )
+    // }
 
     return (
         <>
